4d5c5fa6da4c5b8262ba837d4eef1cfce2014ab0
Sreevatsav-Alladi-Code <srvald01@gmail.com>
2024-02-11 20:11:44 -0500

    code broken

--- src/main/java/frc/robot/commands/PivotArm/armToAmpShotAngle.java
+++ src/main/java/frc/robot/commands/PivotArm/armToAmpShotAngle.java
@@ -39,6 +39,6 @@ public class armToAmpShotAngle extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return (m_PivotArm.isArmAtPosition());
+    return (m_PivotArm.isArmAtPosition(ArmConstants.kAmpAngle));
   }
 }
--- src/main/java/frc/robot/commands/PivotArm/armToFartherShotAngle.java
+++ src/main/java/frc/robot/commands/PivotArm/armToFartherShotAngle.java
@@ -39,6 +39,6 @@ public class armToFartherShotAngle extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return (m_PivotArm.isArmAtPosition());
+    return (m_PivotArm.isArmAtPosition(ArmConstants.kFartherShotAngle));
   }
 }
